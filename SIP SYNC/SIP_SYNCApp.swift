26e7467e16feec148c9486ca72079affb03fbea2
Apple Research Kit <diop@kairosresearch.co>
2025-10-23 11:59:10 -0400

    Initial Commit

---
+++ SIP SYNC/SIP_SYNCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SIP_SYNCApp.swift
+//  SIP SYNC
+//
+//  Created by Diop Shumake on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SIP_SYNCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
